73880f0a9cceca82acdeaad8f72f3a78f72fdb59
David Lim <nicholai.david@gmail.com>
2017-07-15 12:05:46 +0800

    Add fix for trailing slashes

--- app/app.module.js
+++ app/app.module.js
@@ -23,6 +23,17 @@
     routes.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 
     function routes($stateProvider, $urlRouterProvider, $locationProvider) {
+        // Block of code below deals with trailing slashes in URL
+        $urlRouterProvider.rule(function($injector, $location) {
+            var path = $location.path();
+            var hasTrailingSlash = path[path.length-1] === '/';
+
+            if(hasTrailingSlash) {
+                var newPath = path.substr(0, path.length - 1); 
+                return newPath; 
+            } 
+        });
+
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
@@ -34,11 +45,13 @@
             })
             .state('about', {
                 url: '/about',
-                templateUrl: 'views/about.html',
+                templateUrl: 'about/about.view.html',
                 controller: 'AboutController',
                 controllerAs: 'vm'
             })
             ;
+
+        $locationProvider.html5Mode(true);
 
         console.debug('routes loaded.');
     }
